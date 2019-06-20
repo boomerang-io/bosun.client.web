138759335d15c72ef732336e02db1d55243acedb
Timothy R. Bula <trbula@us.ibm.com>
2019-06-20 17:06:59 -0400

    feat: add root level error boundary

--- src/Root.js
+++ src/Root.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import ErrorBoundary from "@boomerang/boomerang-components/lib/ErrorBoundary";
+import ErrorDragon from "Components/ErrorDragon";
 import App from "Features/App";
 import { APP_ROOT } from "Config/appConfig";
 
@@ -13,11 +15,13 @@ export default class Root extends Component {
   render() {
     const { store } = this.props;
     return (
-      <Provider store={store}>
-        <BrowserRouter basename={APP_ROOT}>
-          <App />
-        </BrowserRouter>
-      </Provider>
+      <ErrorBoundary errorComponent={ErrorDragon}>
+        <Provider store={store}>
+          <BrowserRouter basename={APP_ROOT}>
+            <App />
+          </BrowserRouter>
+        </Provider>
+      </ErrorBoundary>
     );
   }
 }
