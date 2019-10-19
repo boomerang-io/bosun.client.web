f4c5fde27bff28057df07267c2c11990ce4b9b26
Timothy R. Bula <trbula@us.ibm.com>
2019-10-18 20:23:09 -0400

    test: update some things

--- src/features/Overview/TeamSelector/index.spec.js
+++ src/features/Overview/TeamSelector/index.spec.js
@@ -1,52 +1,40 @@
 import React from "react";
 import { shallow } from "enzyme";
 import renderer from "react-test-renderer";
-import { Header } from "./index";
+import { TeamSelector } from "./index";
 
 const mockfn = jest.fn();
 const teams = [
   {
-    "id": "5a8b331e262a70306622df73",
-    "name": "ATT",
-    "higherLevelGroupId": "59aebcf47424530fce952fa2",
-    "ucdApplicationId": "96c22f37-5443-4bf5-83bb-7ce90deec820",
-    "isActive": true,
-    "audits": [
+    id: "5a8b331e262a70306622df73",
+    name: "ATT",
+    higherLevelGroupId: "59aebcf47424530fce952fa2",
+    ucdApplicationId: "96c22f37-5443-4bf5-83bb-7ce90deec820",
+    isActive: true,
+    audits: [
       {
-        "auditerId": null,
-        "date": 1519072030853,
-        "note": "CI-Team created"
+        auditerId: null,
+        date: 1519072030853,
+        note: "CI-Team created"
       }
     ],
-    "boomerangTeamName": "AT&T MIL Mobile@Scale",
-    "boomerangTeamShortname": "ms-att-mil"
+    boomerangTeamName: "AT&T MIL Mobile@Scale",
+    boomerangTeamShortname: "ms-att-mil"
   }
 ];
 
-describe("Header --- Snapshot", () => {
-  it("Capturing Snapshot of Header", () => {
-    const renderedValue = renderer
-      .create(
-        <Header
-          handleChangeTeam={mockfn}
-          teams={teams}
-        />
-      )
-      .toJSON();
+describe("TeamSelector --- Snapshot", () => {
+  it("Capturing Snapshot of TeamSelector", () => {
+    const renderedValue = renderer.create(<TeamSelector handleChangeTeam={mockfn} teams={teams} />).toJSON();
     expect(renderedValue).toMatchSnapshot();
   });
 });
 
-describe("Header --- Shallow render", () => {
+describe("TeamSelector --- Shallow render", () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(
-      <Header
-        handleChangeTeam={mockfn}
-        teams={teams}
-      />
-    );
+    wrapper = shallow(<TeamSelector handleChangeTeam={mockfn} teams={teams} />);
   });
 
   it("Render the DUMB component", () => {
