bc1e014ab8cededc034939d383181078c8e70de7
Timothy R. Bula <trbula@us.ibm.com>
2019-10-29 10:55:33 -0400

    fix: only use product url

--- src/components/ErrorDragon/index.js
+++ src/components/ErrorDragon/index.js
@@ -4,10 +4,10 @@ import cx from "classnames";
 import ErrorGraphic from "./ErrorGraphic.js";
 import styles from "./errorDragon.module.scss";
 
-const ErrorDragon = ({ className, style, statusUrl, ...rest }) => {
+const ErrorDragon = ({ className, style, ...rest }) => {
   return (
     <div className={cx(styles.container, className)} style={style} {...rest}>
-      <ErrorGraphic className={styles.graphic} alt="dragon" />
+      <ErrorGraphic className={styles.graphic} alt="Error dragon" />
       <h1 className={styles.title}>Don’t lose your daks</h1>
       <p className={styles.text}>Cheers! You found an error. Try reloading the page.</p>
       <p className={styles.text}>And if you could be so kind, please send us a bug report.</p>
@@ -21,8 +21,7 @@ ErrorDragon.defaultProps = {
 
 ErrorDragon.propTypes = {
   className: PropTypes.string,
-  style: PropTypes.object,
-  statusUrl: PropTypes.string.isRequired
+  style: PropTypes.object
 };
 
 export default ErrorDragon;
--- src/config/axiosGlobalConfig.js
+++ src/config/axiosGlobalConfig.js
@@ -1,5 +1,5 @@
 // Setup global config for Axios requests so they work with ICP
 import axios from "axios";
-import { BASE_SERVICE_ENV_URL } from "./servicesConfig";
+import { PRODUCT_SERVICE_ENV_URL } from "./servicesConfig";
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = BASE_SERVICE_ENV_URL;
+axios.defaults.baseURL = PRODUCT_SERVICE_ENV_URL;
