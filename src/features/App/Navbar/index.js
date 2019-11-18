0df9e6bbe150d42c4960ad49703c026a663d4350
Timothy R. Bula <trbula@us.ibm.com>
2019-11-18 16:51:31 -0500

    fix: sidenav link

--- src/features/App/Navbar/index.js
+++ src/features/App/Navbar/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { LeftSideNav, UIShell } from "@boomerang/carbon-addons-boomerang-react";
 import { BASE_APPS_ENV_URL, BASE_LAUNCH_ENV_URL } from "config/platformUrlConfig";
 import { BASE_SERVICE_ENV_URL } from "config/servicesConfig";
@@ -16,51 +16,6 @@ import { SideNav, SideNavLink, SideNavItems, SideNavMenu, SideNavMenuItem } from
 
 const ACTIVE_CLASS_NAME = "bx--side-nav__link--current";
 
-const onMenuClick = ({ isOpen, onMenuClose }) => (
-  <LeftSideNav isOpen={isOpen}>
-    <SideNav expanded={isOpen} isChildOfHeader={true}>
-      <SideNavItems>
-        <SideNavLink large onClick={onMenuClose} href={`${BASE_APPS_ENV_URL}/ci/`} renderIcon={Integration16}>
-          Components
-        </SideNavLink>
-        <SideNavLink large onClick={onMenuClose} href={`${BASE_APPS_ENV_URL}/scorecard/`} renderIcon={ReportData16}>
-          Scorecard
-        </SideNavLink>
-        <SideNavLink large onClick={onMenuClose} href={`${BASE_APPS_ENV_URL}/insights/`} renderIcon={ChartScatter16}>
-          Insights
-        </SideNavLink>
-        <SideNavLink large onClick={onMenuClose} href={`${BASE_APPS_ENV_URL}/lib/`} renderIcon={Package16}>
-          Lib
-        </SideNavLink>
-        <SideNavLink
-          large
-          activeClassName={ACTIVE_CLASS_NAME}
-          element={NavLink}
-          onClick={onMenuClose}
-          renderIcon={IbmSecurity16}
-          to="/"
-        >
-          Policies
-        </SideNavLink>
-        <SideNavMenu large title="Manage" renderIcon={SettingsAdjust16}>
-          <SideNavMenuItem large onClick={onMenuClose} href={`${BASE_APPS_ENV_URL}/ci/properties`}>
-            Properties
-          </SideNavMenuItem>
-          <SideNavMenuItem large onClick={onMenuClose} href={`${BASE_APPS_ENV_URL}/ci/team-properties`}>
-            Team Properties
-          </SideNavMenuItem>
-          <SideNavMenuItem large onClick={onMenuClose} href={`${BASE_APPS_ENV_URL}/ci/repositories`}>
-            Repositories
-          </SideNavMenuItem>
-          <SideNavMenuItem large onClick={onMenuClose} href={`${BASE_APPS_ENV_URL}/ci/component-modes`}>
-            Modes
-          </SideNavMenuItem>
-        </SideNavMenu>
-      </SideNavItems>
-    </SideNav>
-  </LeftSideNav>
-);
-
 const defaultUIShellProps = {
   baseLaunchEnvUrl: BASE_LAUNCH_ENV_URL,
   baseServiceUrl: BASE_SERVICE_ENV_URL,
@@ -73,18 +28,80 @@ Navbar.propTypes = {
 };
 
 function Navbar({ navigationState, userState }) {
-  if (navigationState.data && userState.data) {
-    return (
-      <UIShell
-        {...defaultUIShellProps}
-        headerConfig={navigationState.data}
-        onMenuClick={onMenuClick}
-        user={userState.data}
-      />
-    );
-  }
-
-  return <UIShell {...defaultUIShellProps} onMenuClick={onMenuClick} />;
+  const location = useLocation();
+  console.log(location);
+  return (
+    <UIShell
+      {...defaultUIShellProps}
+      headerConfig={navigationState?.data ?? {}}
+      onMenuClick={({ isOpen, onMenuClose }) => (
+        <LeftSideNav isOpen={isOpen}>
+          <SideNav expanded={isOpen} isChildOfHeader={true}>
+            <SideNavItems>
+              <SideNavLink large onClick={onMenuClose} href={`${BASE_APPS_ENV_URL}/ci/`} renderIcon={Integration16}>
+                Components
+              </SideNavLink>
+              <SideNavLink
+                large
+                onClick={onMenuClose}
+                href={`${BASE_APPS_ENV_URL}/scorecard/`}
+                renderIcon={ReportData16}
+              >
+                Scorecard
+              </SideNavLink>
+              <SideNavLink
+                large
+                onClick={onMenuClose}
+                href={`${BASE_APPS_ENV_URL}/insights/`}
+                renderIcon={ChartScatter16}
+              >
+                Insights
+              </SideNavLink>
+              <SideNavLink large onClick={onMenuClose} href={`${BASE_APPS_ENV_URL}/lib/`} renderIcon={Package16}>
+                Lib
+              </SideNavLink>
+              <SideNavLink
+                exact
+                large
+                activeClassName={ACTIVE_CLASS_NAME}
+                element={NavLink}
+                isActive={!location.pathname.startsWith("/templates")}
+                onClick={onMenuClose}
+                renderIcon={IbmSecurity16}
+                to="/"
+              >
+                Policies
+              </SideNavLink>
+              <SideNavMenu large title="Manage" renderIcon={SettingsAdjust16}>
+                <SideNavMenuItem large onClick={onMenuClose} href={`${BASE_APPS_ENV_URL}/ci/properties`}>
+                  Properties
+                </SideNavMenuItem>
+                <SideNavMenuItem large onClick={onMenuClose} href={`${BASE_APPS_ENV_URL}/ci/team-properties`}>
+                  Team Properties
+                </SideNavMenuItem>
+                <SideNavMenuItem large onClick={onMenuClose} href={`${BASE_APPS_ENV_URL}/ci/repositories`}>
+                  Repositories
+                </SideNavMenuItem>
+                <SideNavMenuItem large onClick={onMenuClose} href={`${BASE_APPS_ENV_URL}/ci/component-modes`}>
+                  Modes
+                </SideNavMenuItem>
+                <SideNavMenuItem
+                  large
+                  activeClassName={ACTIVE_CLASS_NAME}
+                  element={NavLink}
+                  onClick={onMenuClose}
+                  to={`/templates`}
+                >
+                  Policy Templates
+                </SideNavMenuItem>
+              </SideNavMenu>
+            </SideNavItems>
+          </SideNav>
+        </LeftSideNav>
+      )}
+      user={userState?.data ?? {}}
+    />
+  );
 }
 
 export default Navbar;
