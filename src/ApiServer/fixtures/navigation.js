f5bb0ce1350ceba63e29281e08c091a68cca06a2
Ben Ruby <benjamin.ruby@ibm.com>
2021-07-27 10:28:35 -0600

    chore: clean up and upate packages

--- src/ApiServer/fixtures/navigation.js
+++ src/ApiServer/fixtures/navigation.js
@@ -3,21 +3,18 @@ const navigation = {
   navigation: [
     {
       name: "Launchpad",
-      url: "https://launch.boomerangplatform.net/launchpad",
+      url: "${BASE_LAUNCH_ENV_URL}/launchpad",
     },
     {
       name: "Status",
-      // eslint-disable-next-line
       url: "${BASE_LAUNCH_ENV_URL}/status",
     },
     {
       name: "Docs",
-      // eslint-disable-next-line
       url: "${BASE_LAUNCH_ENV_URL}/docs",
     },
     {
       name: "Admin",
-      // eslint-disable-next-line
       url: "${BASE_LAUNCH_ENV_URL}/admin",
     },
   ],
